151e3b97a680cfa3101c3073541821a6ca67eb4a
Martinica Andrei <d3v1lm41cr1@outlook.com>
2023-06-04 12:19:25 +0300

    added main menu

---
+++ src/components/Grid.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Grid extends React.Component{
+
+    constructor(props : {}){
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    render(): React.ReactNode {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Grid;
